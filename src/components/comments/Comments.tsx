da65991684a76b16d0be986c02dfc064b4e3708d
starlinlq <67881697+starlinlq@users.noreply.github.com>
2021-07-21 12:15:56 -0400

    deleting comments

--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -1,23 +1,31 @@
 import { observer } from "mobx-react-lite";
-import { Fragment } from "react";
-import { useStore } from "../../app/stores/stores";
-import CreateComment from "./CreateComment";
 import { Comment } from "../../app/interfaces";
-import { user } from "../../app/api/agent";
 import { history } from "../..";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 const Comments = ({ id, user_id, comment, user_name }: Comment) => {
   const handleClick = () => {
     history.push(`/profile/${user_id}`);
   };
+
+  const handleDelete = (comment_id:string) =>{
+    
+
+
+  }
   return (
     <div className="display_comment">
-      <div className="l_name">{user_name.slice(0, 1)}</div>
+      <div className="l_name">
+        <p>{user_name.slice(0, 1)}</p>
+      </div>
       <div className="content">
         <div className="author">
           <button onClick={handleClick} className="author_name" type="button">
             {user_name}
           </button>
+          <div className="icon">
+            <RiDeleteBin6Line />
+          </div>
         </div>
         <div className="comment">{comment}</div>
       </div>
